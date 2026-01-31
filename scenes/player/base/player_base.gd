9f2f9d4d5104b73fa475153dd8bfcca627bfd70a
Pablo Gallina <pablogallinag@gmail.com>
2026-01-31 12:19:22 -0600

    add gravity & jump

--- scenes/player/base/player_base.gd
+++ scenes/player/base/player_base.gd
@@ -1,13 +1,30 @@
 class_name PlayerBase
 extends CharacterBody2D
 
-var speed : float = 200
+@export var speed: float = 450.0
+@export var gravity: float = 3000.0
+@export var jump_force: float = -1200.0
 @export var input_prefix: String = "p1"
 
-func _process(_delta: float) -> void:
-	_motion_ctrl()
-
-func _motion_ctrl():
-	var dir_x = Input.get_axis(input_prefix + "_left", input_prefix + "_right")
-	velocity.x = dir_x * speed
+func _physics_process(delta: float) -> void:
+	_apply_gravity(delta)
+	_handle_jump()
+	_handle_movement()
 	move_and_slide()
+
+func _apply_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity.y = 0.0
+
+func _handle_jump() -> void:
+	if is_on_floor() and Input.is_action_just_pressed(input_prefix + "_jump"):
+		velocity.y = jump_force
+
+func _handle_movement() -> void:
+	var dir_x := Input.get_axis(
+		input_prefix + "_left",
+		input_prefix + "_right"
+	)
+	velocity.x = dir_x * speed
