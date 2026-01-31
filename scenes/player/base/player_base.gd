351a087f6c1a829e07acd8dc43c098be8c5d5f43
Daniel <danieljgallina@gmail.com>
2026-01-31 11:48:53 -0600

    merge

--- scenes/player/base/player_base.gd
+++ scenes/player/base/player_base.gd
@@ -2,11 +2,12 @@ class_name PlayerBase
 extends CharacterBody2D
 
 var speed : float = 200
+@export var input_prefix: String = "p1"
 
 func _process(_delta: float) -> void:
 	_motion_ctrl()
 
 func _motion_ctrl():
-	velocity.x = GLOBAL.get_axis().x * speed
-	#velocity.y = GLOBAL.get_axis().y * speed
+	var dir_x = Input.get_axis(input_prefix + "_left", input_prefix + "_right")
+	velocity.x = dir_x * speed
 	move_and_slide()
