57d9aec26f7edc5b30cad3c5291d23f3d41aa898
Pablo Gallina <pablogallinag@gmail.com>
2026-01-31 18:10:38 -0600

    add rock hitbox

--- scenes/player/base/hitbox_base.gd
+++ scenes/player/base/hitbox_base.gd
@@ -1,14 +1,13 @@
 class_name HitboxBase
 extends Area2D
 
-@export var damage: int = 1
+@export var damage: int = 25
 
 func _ready() -> void:
 	monitoring = false
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
-	print("entramos")
 	if body.has_method("take_damage"):
 		body.take_damage(damage)
 
--- scenes/player/rock/rock.gd
+++ scenes/player/rock/rock.gd
@@ -1,5 +1,7 @@
 extends PlayerBase
 
+const CHARACTER = GLOBAL.CHARACTERS_TYPES.ROCK
+
 func _ready() -> void:
 	print("Soy una roca")
 	speed = 200
