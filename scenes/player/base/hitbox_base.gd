70141a5fe03e5d44ac8aeacbfcbdd0c24a2cbb6d
Daniel <danieljgallina@gmail.com>
2026-02-01 14:25:20 -0600

    fixes

--- scenes/player/base/hitbox_base.gd
+++ scenes/player/base/hitbox_base.gd
@@ -2,16 +2,21 @@ class_name HitboxBase
 extends Area2D
 
 @export var damage: int = 25
+var _has_hit := false
 
 func _ready() -> void:
 	monitoring = false
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
+	if _has_hit:
+		return
 	if body.has_method("take_damage"):
+		_has_hit = true
 		body.take_damage(damage)
 
 func enable() -> void:
+	_has_hit = false
 	monitoring = true
 
 func disable() -> void:
